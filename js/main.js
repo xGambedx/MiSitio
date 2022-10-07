9165ee100e0364ba542e8a5202e7ae7256c46323
xGambedx <108758310+xGambedx@users.noreply.github.com>
2022-10-06 21:01:06 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 $(function(){
     $("section").each(function(){ 
-        var tituloSeccion = $(this).find("h2").text() 
+        let tituloSeccion = $(this).find("h2").text() 
         console.log(tituloSeccion);
-        var idSeccion = $(this).attr("id");
-
+        let idSeccion = $(this).attr("id");
         $("#main-menu").append('<li class="nav-item">'
             +'<a class="nav-link" href="#'+ idSeccion +'">'+ tituloSeccion +'</a>'
             +'</li>');
     })
+
+    $("section").each(function(){
+        let ocultar = true;
+        let seccion = $(this).attr("id");
+        $("#"+seccion+" h2").click(function(){
+            if (ocultar === true) {
+                $("#"+seccion+" .ocultar").hide(1000);
+                ocultar = false;
+            } else {
+                $("#"+seccion+" .ocultar").show("slow");
+                ocultar = true;
+            }
+        });
+    })
+
+    $("#menu-item-ayuda").click(function(){
+        fetch("herramientas/ayuda.html")
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                $("#main-content").html(data);
+            });
+    })
+
+    $("#menu-item-ranking").click(function(){
+        fetch("herramientas/ranking.html")
+            .then(response => response.text())
+            .then(data => {
+                console.log(data)
+                $("#main-content").html(data);
+            });
+    })
+    
 })
